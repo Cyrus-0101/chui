771afd6e82285157ca0e67075d8511ee7e505db4
Cyrus-0101 <cyrus_g@outlook.com>
2024-05-18 09:50:47 +0300

    Added chui name to the top of the repl

--- src/main.go
+++ src/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"chui/repl"
+	"fmt"
 	"os"
 	"os/user"
 )
 
 func main() {
 	user, err := user.Current()
+
 	if err != nil {
 		panic(err)
 	}
+	const chui_FACE = `
+	░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓█▓▒░      ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+	░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░                                  
+	`
+	fmt.Printf("%s\n", chui_FACE)
+
 	fmt.Printf("Hello %s! This is the chui programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
